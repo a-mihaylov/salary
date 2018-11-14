9394db74de2256a6240ce670cb17da88d03937ee
Mikhail Kovalev <kovalev.m@volgablob.ru>
2018-11-14 20:10:16 +0400

    Убрано ограничение даты на максимум

--- salary/prikazCreate.cpp
+++ salary/prikazCreate.cpp
@@ -5,7 +5,6 @@ PrikazCreate::PrikazCreate(QWidget *parent)
     : QDialog(parent) {
   prikazui.setupUi(this);
   createButtonClicked = false;
-  prikazui.prikaz_create_date->setMaximumDate(QDate::currentDate());
   prikazui.prikaz_create_date->setDate(QDate::currentDate());
   bool a = connect(prikazui.prikaz_create_create, SIGNAL(clicked()), this, SLOT(createPrikazResult()));
   bool c = connect(prikazui.prikaz_create_cancel, SIGNAL(clicked()), this, SLOT(canselPrikazCreate()));
