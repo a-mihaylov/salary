fa85e6b231ab7b7fcfe207c57202466448bc383d
Mikhail Kovalev <kovalev.m@volgablob.ru>
2018-11-14 17:22:51 +0400

    Добавлен Fusion стиль

--- salary/main.cpp
+++ salary/main.cpp
@@ -1,10 +1,12 @@
 #include "salary.h"
 #include <QtWidgets/QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
   a.setWindowIcon(QIcon("images/icon.png"));
+  QApplication::setStyle(QStyleFactory::create("Fusion"));
 	salary w;
 	w.show();
 	return a.exec();
